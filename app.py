c1e71074f39a8c7668e7a45280cb3821cd97cf8b
Gregory Howlett-Gomez <ghowlettgomez@gmail.com>
2018-02-11 07:52:24 -0600

    changed send format to json

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 import json 
 from weather import Weather
 import requests
@@ -13,19 +13,19 @@ def hello():
 def get_weather(location):
 	try:
 		weather_text = Weather().lookup_by_location(location).condition().text()
-		return "The weather in " + str(location) + " is " + weather_text + "."
+		return {'payload': "The weather in " + str(location) + " is " + weather_text + "."}
 	except Exception, e:
-		return '"There is no data about that location currently."'
+		return jsonify({'payload': 'There is no data about that location currently.'})
 
 @app.route("/get_crypto_price/<currency>")
 def get_crypto(currency):
 	try:
 		query_url = 'https://api.coinmarketcap.com/v1/ticker/' + currency + '/'
 		currency_data = requests.get(query_url).json()[0]
-		return '"The current price of ' + currency_data['name'] + " is $" + currency_data['price_usd'] + '."' 
+		return jsonify({'payload': 'The current price of ' + currency_data['name'] + " is $" + currency_data['price_usd'] + '.' })
 	except Exception, e:
 		return e
-		return '"We do not have information for that currency."'
+		return jsonify({'payload': 'We do not have information for that currency.'})
 
 @app.route('/get_business_name/<keyword>/<location>')
 def get_business(keyword, location):
@@ -39,14 +39,14 @@ def get_business(keyword, location):
 			'authorization': 'Bearer LShSIBnaqSv6ybcTWZV9P4eObV157TU5gsfXHU-SOD3nvDvYP6q7JD8KBpKS2Py-nN1FHXc6ONAPc65XPiCpJgmp1vHDmEfu2UAhzK3go8CRD9WFqMAa4EPeah6AWnYx'
 		}
 		response = requests.get(search_term, params=params, headers=headers).json()
-		res = '"'
+		res = ''
 		for business in response['businesses']:
 			res += business['name'] + ', '
-		return res + '"'
+		return jsonify({'payload': res})
 	except Exception, e:
 		return e
-		return '"We do not have information about anything about that."'
+		return jsonify({'payload' : 'We do not have information about anything about that.'})
 
 @app.route('/tell_a_joke')
 def tell_a_joke():
-	return '"You should invest in bitcoin."'
+	return jsonify({'payload' : 'You should invest in bitcoin.'})
