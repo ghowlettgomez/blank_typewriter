e8505c1bcfeb22c68bbd58b3442b9676e8489b8c
Gregory Howlett-Gomez <ghowlettgomez@gmail.com>
2018-02-11 05:09:33 -0600

    added yelp to api

--- app.py
+++ app.py
@@ -24,5 +24,29 @@ def get_crypto(currency):
 		currency_data = requests.get(query_url).json()[0]
 		return "The current price of " + currency_data['name'] + " is $" + currency_data['price_usd'] + "." 
 	except Exception, e:
+		return e
 		return "We do not have information for that currency."
 
+@app.route('/get_business_name/<keyword>/<location>')
+def get_business(keyword, location):
+	try:
+		search_term = 'https://api.yelp.com/v3/businesses/search'
+		params = {
+			'term': str(keyword),
+			'location': location,
+		}
+		headers = {
+			'authorization': 'Bearer LShSIBnaqSv6ybcTWZV9P4eObV157TU5gsfXHU-SOD3nvDvYP6q7JD8KBpKS2Py-nN1FHXc6ONAPc65XPiCpJgmp1vHDmEfu2UAhzK3go8CRD9WFqMAa4EPeah6AWnYx'
+		}
+		response = requests.get(search_term, params=params, headers=headers).json()
+		res = ''
+		for business in response['businesses']:
+			res += business['name'] + ', '
+		return res 
+	except Exception, e:
+		return e
+		return 'We do not have information about anything about that.'
+
+@app.route('/tell_a_joke')
+def tell_a_joke():
+	return 'You should invest in bitcoin.'
